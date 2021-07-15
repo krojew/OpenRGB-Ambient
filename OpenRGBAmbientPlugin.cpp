a67b538b55da37831bfa838ebf1f8c7da91d8649
Kamil Rojewski <kamil.rojewski@gmail.com>
2021-07-15 07:28:41 +0200

    build fix

--- OpenRGBAmbientPlugin.cpp
+++ OpenRGBAmbientPlugin.cpp
@@ -1,7 +1,8 @@
 //
 // Created by Kamil Rojewski on 15.07.2021.
 //
-#include "OpenRGBAmbientPlugin.h
+
+#include "OpenRGBAmbientPlugin.h"
 
 OpenRGBPluginInfo OpenRGBAmbientPlugin::Initialize(bool dark_theme, ResourceManager *resource_manager_ptr) {
     return OpenRGBPluginInfo();

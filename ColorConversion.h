db3de70a31189b3a777e51e4e11b2b73262ca9f5
Kamil Rojewski <kamil.rojewski@gmail.com>
2021-07-20 21:04:36 +0200

    downscaled blue more

--- ColorConversion.h
+++ ColorConversion.h
@@ -6,6 +6,6 @@
 #define OPENRGB_AMBIENT_COLORCONVERSION_H
 
 // HUE 2 LEDs are cool white, so simply scale down blue a bit
-const auto blueScale = 0.7f;
+const auto blueScale = 0.6f;
 
 #endif //OPENRGB_AMBIENT_COLORCONVERSION_H

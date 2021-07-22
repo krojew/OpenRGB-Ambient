b7523daeaa28256fbf4f73093ab53a3f620f748e
Kamil Rojewski <kamil.rojewski@gmail.com>
2021-07-22 07:16:37 +0200

    removed normalized hdr

--- HdrHorizontalRegionProcessor.cpp
+++ HdrHorizontalRegionProcessor.cpp
@@ -16,47 +16,6 @@ void HdrHorizontalRegionProcessor::processRegion(RGBColor *result, const std::ui
     const auto sampleWidth = width / samples;
     const auto samplePixels = sampleWidth * height;
 
-#ifdef NORMALIZED_HDR
-
-    // Converting HDR to SDR is non-trivial and requires tone mapping with information about maximum luminance in given
-    // frame. Here a workaround simply tries to avoid all that and preserve relative color distribution to at least
-    // look similar.
-
-    for (auto sample = 0; sample < samples; ++sample)
-    {
-        uint red = 0;
-        uint green = 0;
-        uint blue = 0;
-
-        ushort maxColor = 255;
-
-        for (auto y = 0; y < height; ++y)
-        {
-            const auto currentWidth = (sample + 1) * sampleWidth;
-            for (auto x = sample * sampleWidth; x < currentWidth; ++x)
-            {
-                const auto colors = data[y * width + x];
-                const ushort curRed = colors & 0x3ff;
-                const ushort curGreen = (colors >> 10) & 0x3ff;
-                const ushort curBlue = (colors >> 20) & 0x3ff;
-
-                red += curRed;
-                green += curGreen;
-                blue += curBlue;
-
-                maxColor = std::max({maxColor, curRed, curBlue, curGreen});
-            }
-        }
-
-        result[samples - sample - 1] = ToRGBColor(
-                red / samplePixels * 255 / maxColor,
-                green / samplePixels * 255 / maxColor,
-                static_cast<uchar>(blue * blueScale / samplePixels * 255 / maxColor)
-        );
-    }
-
-#else
-
     for (auto sample = 0; sample < samples; ++sample)
     {
         uint red = 0;
@@ -85,6 +44,4 @@ void HdrHorizontalRegionProcessor::processRegion(RGBColor *result, const std::ui
                 static_cast<uchar>(blue * blueScale / samplePixels / 4)
         );
     }
-
-#endif
 }
--- HdrVerticalRegionProcessor.cpp
+++ HdrVerticalRegionProcessor.cpp
@@ -16,48 +16,6 @@ void HdrVerticalRegionProcessor::processRegion(RGBColor *result, const std::uint
     const auto sampleHeight = height / samples;
     const auto samplePixels = sampleHeight * width;
 
-#ifdef NORMALIZED_HDR
-
-    // Converting HDR to SDR is non-trivial and requires tone mapping with information about maximum luminance in given
-    // frame. Here a workaround simply tries to avoid all that and preserve relative color distribution to at least
-    // look similar.
-
-    for (auto sample = 0; sample < samples; ++sample)
-    {
-        uint red = 0;
-        uint green = 0;
-        uint blue = 0;
-
-        ushort maxColor = 255;
-
-        const auto currentHeight = (sample + 1) * sampleHeight;
-        for (auto y = sample * sampleHeight; y < currentHeight; ++y)
-        {
-            const auto endX = startX + width;
-            for (auto x = startX; x < endX; ++x)
-            {
-                const auto colors = data[y * realWidth + x];
-                const ushort curRed = colors & 0x3ff;
-                const ushort curGreen = (colors >> 10) & 0x3ff;
-                const ushort curBlue = (colors >> 20) & 0x3ff;
-
-                red += curRed;
-                green += curGreen;
-                blue += curBlue;
-
-                maxColor = std::max({maxColor, curRed, curBlue, curGreen});
-            }
-        }
-
-        result[samples - sample - 1] = ToRGBColor(
-                red / samplePixels * 255 / maxColor,
-                green / samplePixels * 255 / maxColor,
-                static_cast<uchar>(blue * blueScale / samplePixels * 255 / maxColor)
-        );
-    }
-
-#else
-
     for (auto sample = 0; sample < samples; ++sample)
     {
         uint red = 0;
@@ -87,6 +45,4 @@ void HdrVerticalRegionProcessor::processRegion(RGBColor *result, const std::uint
                 static_cast<uchar>(blue * blueScale / samplePixels / 4)
         );
     }
-
-#endif
 }
